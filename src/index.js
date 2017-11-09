16ca9e09f613924c79ff5a6debbff458d379c80f
董政凯 <dongzhk@yonyou.com>
2017-11-09 22:22:16 +0800

    [add] add init command and init.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,23 @@
  * @last modified time: 2017-11-06 18:10:00
  */
 
-var chalk = require('chalk');
+const chalk = require('chalk');
 const build = require('../lib/build');
 const create = require('../lib/create');
+const init = require('../lib/init');
 const program = require('commander');
 
 // 获取当前node版本，如果小于6，则无法使用
-var currentNodeVersion = process.versions.node;
+const currentNodeVersion = process.versions.node;
 if (currentNodeVersion.split('.')[0] < 6) {
     console.error(chalk.red('You are running Node ' + currentNodeVersion + '.\n' + 'Create Uba App requires Node 6 or higher. \n' + 'Please update your version of Node.'));
     process.exit(1);
 }
+
+const moliInfo = require('./../package.json');
+
+program
+    .version(moliInfo.version);
 
 program
     .command('create <projectName>')
@@ -24,11 +30,13 @@ program
         create.run(projectName);
     });
 
-var moliInfo = require('./../package.json');
-
 program
-    .version(moliInfo.version);
-
+    .command('init')
+    .description("init your npm env")
+    .action(function () {
+        // this is init project,download npm module
+        init.download();
+    });
 program
     .usage('')
     .command('build <platform>')
@@ -37,8 +45,8 @@ program
     .option("--debug", "debug mode")
     .description('build your project with android/ios')
     .action(function (platform, options) {
-        var buildType = options.type;
-        if ("ios" == platform || "android" == platform) {
+        const buildType = options.type;
+        if (platform == "ios" || platform == "android") {
             console.log("platform " + platform + " buildType " + buildType);
             if (options.release) {
                 console.log("this release build");
